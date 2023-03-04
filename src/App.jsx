7c9dfa533e9224f45369a70ff34a01f3c4d1c0ca
Yash-Sajwan24 <yashsajwan12345@gmail.com>
2023-03-04 19:45:11 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,6 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
